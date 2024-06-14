4532b91ed9fe3a6f1fdb57b051d4e701d695586d
Selvakumar Arumugam <selvait90@gmail.com>
2024-06-14 09:44:47 +0530

    Fetch patient data from FHIR Patient API

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,50 @@
-export default function Home() {
+async function getData() {
+  // Configure the URL to fetch specific patient data
+  // Application will throw error if patient doesn't exists in FHIR Server
+  const res = await fetch('https://hapi.fhir.org/baseR4/Patient/44707296?_format=json')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export default async function Home() {
+  const data = await getData()
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          FHIR API based Patient Application
+    <main className="p-24">
+      <div className="w-full font-mono">
+        <p>
+          <b><u>FHIR API based Patient Application</u></b>
         </p>
+        <table>
+          <tr>
+            <td>First Name</td>
+            <td>{data.name[0].given}</td>
+          </tr>
+          <tr>
+            <td>Last Name</td>
+            <td>{data.name[0].family}</td>
+          </tr>
+          <tr>
+            <td>Date of Birth</td>
+            <td>{data.birthDate}</td>
+          </tr>
+          <tr>
+            <td>Gender</td>
+            <td>{data.gender}</td>
+          </tr>
+        </table>
+        <br />
+        <p>Patient Data JSON:  <pre>{JSON.stringify(data, null, 2) }</pre></p>
       </div>
+      <div>
+     </div>
     </main>
   );
 }
